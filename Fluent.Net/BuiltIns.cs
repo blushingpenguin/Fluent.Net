3e8bcb16f62ae86606093d593aaa44112a1dd543
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-02-22 17:23:00 +0100

    Move things out of MessageContext.cs
    This file had no business containing all these random types.

---
+++ Fluent.Net/BuiltIns.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fluent.Net
+{
+    internal static class BuiltIns
+    {
+        public static FluentType Number(IList<object> args, IDictionary<string, object> options)
+        {
+            // TODO: add to errors?  what we doin here?
+            if (args.Count != 1)
+            {
+                throw new Exception("Too many arguments to NUMBER() function");
+            }
+            if (args[0].GetType() != typeof(FluentNumber))
+            {
+                throw new Exception("NUMBER() expected an argument of type FluentNumber");
+            }
+            return (FluentNumber)args[0];
+        }
+
+        public static FluentType DateTime(IList<object> args, IDictionary<string, object> options)
+        {
+            if (args.Count != 1)
+            {
+                throw new Exception("Too many arguments to DATETIME() function");
+            }
+            if (args[0].GetType() != typeof(FluentDateTime))
+            {
+                throw new Exception("DATETIME() expected an argument of type FluentDateTime");
+            }
+            return (FluentDateTime)args[0];
+        }
+    }
+}
---
+++ Fluent.Net/FluentError.cs
@@ -0,0 +1,36 @@
+﻿namespace Fluent.Net
+{
+    public abstract class FluentError
+    {
+        public string Message { get; set; }
+
+        public FluentError(string message)
+        {
+            Message = message;
+        }
+    }
+
+    class RangeError : FluentError
+    {
+        public RangeError(string message) :
+            base(message)
+        {
+        }
+    }
+
+    class TypeError : FluentError
+    {
+        public TypeError(string message) :
+            base(message)
+        {
+        }
+    }
+
+    class ReferenceError : FluentError
+    {
+        public ReferenceError(string message) :
+            base(message)
+        {
+        }
+    }
+}
